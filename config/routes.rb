bbd88ed08f465a1a1e476125029d4e0fd40afc94
Olivier <olivier.simart@gmail.com>
2012-04-06 00:35:35 -0400

    commit for nothing

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Moving::Application.routes.draw do
   resources :documents
   resource :account, :only => [:show, :update]
   
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users
   resources :users
   root :to => 'dashboard#show'
 end
