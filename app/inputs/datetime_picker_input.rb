a0c9f97c9d614c843337a5dbc9ce0c3c11ce337a
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-04-17 16:17:29 +0700

    Add minute step for removal time

--- app/inputs/datetime_picker_input.rb
+++ app/inputs/datetime_picker_input.rb
@@ -5,7 +5,7 @@ class DatetimePickerInput < SimpleForm::Inputs::Base
     raw_value = @builder.object.send(attribute_name)
     value = raw_value.blank? ? '' : I18n.l(raw_value.to_date)
     picker = template.text_field_tag("#{object.class.to_s.downcase}[#{attribute_name}][date]", value, class: "datepicker", "data-date-format" => date_format)
-    time = template.select_time(raw_value || nil, :ignore_date => true, :prefix => "#{object.class.to_s.downcase}[#{attribute_name}]")
+    time = template.select_time(raw_value || nil, :minute_step => 15, :ignore_date => true, :prefix => "#{object.class.to_s.downcase}[#{attribute_name}]")
     "#{picker} #{time}".html_safe
   end
 end
