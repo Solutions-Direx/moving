41916faa564d228e87cd3c41810ccf143f693c51
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-04-17 13:19:43 +0700

    Fix subnav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,29 +19,19 @@
 //= require ../../../vendor/assets/javascripts/jquery.dialog2.helpers
 //= require_tree ../../../vendor/assets/javascripts
 //= require_tree .
-$(function() {
-  // fix sub nav on scroll
-  var $win = $(window)
-    , $nav = $('.subnav')
-  , navHeight = $('.navbar').first().height()
-    , navTop = $('.subnav').length && $('.subnav').offset().top - navHeight
-    , isFixed = 0
 
-  processScroll()
-
-  $win.on('scroll', processScroll)
-
-  function processScroll() {
-    var i, scrollTop = $win.scrollTop()
-    if (scrollTop >= navTop && !isFixed) {
-      isFixed = 1
-      $nav.addClass('subnav-fixed')
-    } else if (scrollTop <= navTop && isFixed) {
-      isFixed = 0
-      $nav.removeClass('subnav-fixed')
+$(document).scroll(function(){
+    // If has not activated (has no attribute "data-top"
+    if (!$('.subnav').attr('data-top')) {
+        // If already fixed, then do nothing
+        if ($('.subnav').hasClass('subnav-fixed')) return;
+        // Remember top position
+        var offset = $('.subnav').offset()
+        $('.subnav').attr('data-top', offset.top);
     }
-  }
 
-  $('a[rel=popover]').popover();
-
+    if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
+        $('.subnav').addClass('subnav-fixed');
+    else
+        $('.subnav').removeClass('subnav-fixed');
 });
