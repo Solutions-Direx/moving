1a44205468a0b42a7e8553a84cf6379d7981d6e1
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-04-17 15:07:55 +0700

    Fix subnav again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,18 +20,27 @@
 //= require_tree ../../../vendor/assets/javascripts
 //= require_tree .
 
-$(document).scroll(function(){
-    // If has not activated (has no attribute "data-top"
-    if (!$('.subnav').attr('data-top')) {
-        // If already fixed, then do nothing
-        if ($('.subnav').hasClass('subnav-fixed')) return;
-        // Remember top position
-        var offset = $('.subnav').offset()
-        $('.subnav').attr('data-top', offset.top);
-    }
 
-    if ($('.subnav').attr('data-top') - $('.subnav').outerHeight() <= $(this).scrollTop())
-        $('.subnav').addClass('subnav-fixed');
-    else
-        $('.subnav').removeClass('subnav-fixed');
+$(function() {
+  // fix sub nav on scroll
+      var $win = $(window)
+        , $nav = $('.subnav')
+  	  , navHeight = $('.navbar').first().height()
+        , navTop = $('.subnav').length && $('.subnav').offset().top - navHeight
+        , isFixed = 0
+
+      processScroll()
+
+      $win.on('scroll', processScroll)
+
+      function processScroll() {
+        var i, scrollTop = $win.scrollTop()
+        if (scrollTop >= navTop && !isFixed) {
+          isFixed = 1
+          $nav.addClass('subnav-fixed')
+        } else if (scrollTop <= navTop && isFixed) {
+          isFixed = 0
+          $nav.removeClass('subnav-fixed')
+        }
+      }
 });
