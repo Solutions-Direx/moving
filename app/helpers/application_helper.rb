f7a712f2c4b78eaedc43ae6ecb9d394e13cc421a
Olivier <olivier.simart@gmail.com>
2012-05-02 16:03:00 -0400

    dashboard#show & quotes#index translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   
-  def submit_or_cancel(form, submit_name = "", cancel_name='Cancel')
+  def submit_or_cancel(form, submit_name = "", cancel_name="#{t 'cancel'}")
     unless submit_name.blank?
-      form.submit(submit_name, :class => 'btn btn-primary') + " or " + link_to(cancel_name, 'javascript:history.go(-1);', :class => 'cancel')
+      form.submit(submit_name, :class => 'btn btn-primary') + " #{t 'or'} " + link_to(cancel_name, 'javascript:history.go(-1);', :class => 'cancel')
     else
-      form.submit(:class => 'btn btn-primary') + " or " + link_to(cancel_name, 'javascript:history.go(-1);', :class => 'cancel')
+      form.submit(:class => 'btn btn-primary') + " #{t 'or'} " + link_to(cancel_name, 'javascript:history.go(-1);', :class => 'cancel')
     end
   end
   
