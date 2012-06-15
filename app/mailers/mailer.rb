f73c5d049b1dcddb76d430dc26b6cd5bffa232b4
Olivier <olivier.simart@gmail.com>
2012-06-15 13:18:39 -0400

    Mailer & translation. closed #54

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,23 +2,26 @@
 class Mailer < ActionMailer::Base
   add_template_helper(BootstrapHelper)
   
-  default from: "from@example.com"
+  #default from: "from@example.com"
   
   def user_created_email(user, generated_password)
     @user = user
     @generated_password = generated_password
     
-    mail(to: @user.email, subject: "#{t 'account_created', default: 'Account created'}")
+    mail(from: "#{@user.account.try(:company_name)} <#{@user.account.owner.email}>", to: @user.email, 
+         subject: "#{t 'account_created', default: 'Account created'}")
   end
   
   def quote_email(quote)
     @quote = quote
-    mail(to: @quote.client.email, subject: "#{t 'quote_information', default: 'Quote information'}")
+    mail(from: "#{@quote.creator.account.try(:company_name)} <#{@quote.creator.email}>", to: @quote.client.email, 
+         subject: "#{t 'quote_information', default: 'Quote information'}")
   end
   
   def invoice_email(invoice)
     @quote = invoice.quote
     @invoice = invoice
-    mail(to: @quote.client.email, subject: "#{Invoice.model_name.human}")
+    mail(from: "#{@invoice.quote.creator.account.try(:company_name)} <#{@invoice.quote.creator.email}>", to: @quote.client.email, 
+         subject: "#{Invoice.model_name.human}")
   end
 end
