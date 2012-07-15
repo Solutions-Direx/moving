daa82228fbbb7fb0186b32763f7079e25aef3032
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-07-15 19:40:45 +0700

    Support transaction number for credit/debit

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,4 +6,17 @@ class Payment < ActiveRecord::Base
   validates :amount, :date, :payment_method, :invoice_id, :presence => true
 
   validates :amount, :numericality => { :greater_than => 0 }
+
+  before_save :cleanup
+
+  def cleanup
+    # clear transaction number if not credit/debit
+    unless ["credit", "debit"].include?(payment_method)
+      self.transaction_number = ""
+    end
+    # clear card type if not credit
+    unless payment_method == "credit"
+      self.credit_card_type = ""
+    end
+  end
 end
