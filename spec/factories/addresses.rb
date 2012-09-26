0319653cf5edcc0ca7816232abc3c5a4d66f7bdb
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-09-26 10:43:25 +0700

    Fix faker

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
   factory :address do
     address Faker::Address.street_address
     city Faker::Address.city
-    province Faker::Address.us_state
+    province Faker::Address.state
     postal_code Faker::Address.zip_code
     country "Canada"
   end
