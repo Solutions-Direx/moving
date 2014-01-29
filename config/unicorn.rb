425f63268c6951f7356475fccf720dc13bf8feb1
Gennady Kudelya <gennady.kudelya@gmail.com>
2014-01-29 17:42:59 +0200

    changed nginx and unicorn configurations

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/moving/current"
+root = "/home/deployer/apps/moving_dr/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.moving.sock"
+listen "/tmp/unicorn.moving_dr.sock"
 worker_processes 2
 timeout 30
